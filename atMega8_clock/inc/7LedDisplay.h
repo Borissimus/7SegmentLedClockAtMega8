0a18533db07db5e895c18a03573591add787ae4c
Borys Nykytiuk <borys.nykytiuk@diasemi.com>
2020-10-16 21:28:10 +0300

    Led display.h update

--- atMega8_clock/inc/7LedDisplay.h
+++ atMega8_clock/inc/7LedDisplay.h
@@ -7,24 +7,34 @@
 #ifndef _7LEDDISPLAY_H_
 #define _7LEDDISPLAY_H_
 #endif /* 7LEDDISPLAY_H_ */
+// User includs
 #include <stdint.h>
 
-#define NUM_OF_DIGITS 6
+// User defines
+#define NUM_OF_DIGITS 6 // 7Led display digits quantity
+// 7Seg display type: Common Anode 1 or Common Cathode 0
+#define COMMON_ANODE 1
+#define COMMON_CATHODE 0
+// Display data source defines
+#define GLOBAL_TIME 1   // GlobalTimeData is selected as CuurrSourceData
+#define SETTING_TIME 0  // SettingTimeData is selected as CuurrSourceData 
 
+// current LED (Digit) selector port definition
+// display selector pins must be continuously set from any port (PINB1 - PINB6 for example) 
 typedef struct DigitPointerPort {
-	uint8_t CurrPtrMask;
 	volatile uint8_t *port;
 	uint8_t StatrPinNum;
 } DigitPointerPort;
 
+// Pre define Display prototype 
 typedef struct Led7SegDispProto Display;
 
 typedef struct Led7SegDispProto {
-	uint8_t CurDispObj;                  // 1 - current time, 0 - time setup
-	uint8_t CurDigit;                    // currently displayed digit
+	uint8_t CurrSoucreData;              // disp. src selector GLOBAL_TIME or SETTING_TIME
+	uint8_t CurrDigitNum;                // currently displaying digit number
 	uint8_t NumOfDigits;                 // must be assigned to NUM_OF_DIGITS  
-	uint8_t Digit[NUM_OF_DIGITS];        // display of digits array 
-	DigitPointerPort DigitPointer;       // 
-	void(*setCurrDigit)(Display *disp);  // 
-	pin double_point;
+	uint8_t Digit[NUM_OF_DIGITS];        // display of digits array (display current data) 
+	DigitPointerPort *CurrDigitPointer;  // struct used for setup cur. digit displaying and definition
+	void(*SwitchDigit)(Display *disp);   // Update displayed digit by modifying *CurrDigitPointer 
+	pin double_point;                    // seconds marker - double point (pin object)
 } Display;
